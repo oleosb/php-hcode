d596c7954e8f4ade6f9027be8de030cf471687a7
oleosb <oleosbo@gmail.com>
2023-08-24 21:56:59 -0300

    Include & Require

---
+++ secao3/include/inc-exemplo-01.1.php
@@ -0,0 +1,11 @@
+<?php
+
+	//include "inc/inc-exemplo-01.php";
+	require_once "inc/inc-exemplo-01.php";
+	require_once "inc/inc-exemplo-01.php";
+
+	$resultado = somar(10,20);
+
+	echo $resultado;
+
+  ?>
