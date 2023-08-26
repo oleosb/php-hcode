56d7a157da3537ec71355e385def2a6d7cb5e805
oleosb <oleosbo@gmail.com>
2023-08-25 21:07:48 -0300

    foreach

---
+++ secao4-condicoes-e-repeticoes/cr-exemplo-04.php
@@ -0,0 +1,10 @@
+<?php
+	$meses = array(
+		"Janeiro", "Fevereiro", "Marco", "Abril","Maio", "Junho", "Julho", "Agosto","Setembro", "Outubro", "Novembro", "Dezemnbro"
+	);
+
+	foreach ($meses as $idx => $mes) {
+		echo "Index: ".$idx."<br>";
+		echo "O mes e: ".$mes."<br><br>";
+	}
+?>
