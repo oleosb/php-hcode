170d8bcb8491d6c4855727fb7853e9d8949e8349
oleosb <oleosbo@gmail.com>
2023-09-12 15:43:47 -0300

    Diretorios Virtuais

---
+++ website/index.php
@@ -0,0 +1,26 @@
+<?php
+
+phpinfo();
+
+/*
+
+//Adicionar no https-vhosts.conf
+<VirtualHost *:80>
+    ServerAdmin webmaster@dummy-host2.example.com
+    DocumentRoot "C:/xampp/htdocs/website"
+    ServerName local.cursophp7.com.br
+    ErrorLog "logs/dummy-host2.example.com-error.log"
+    CustomLog "logs/dummy-host2.example.com-access.log" common
+    <Directory "C:/xampp/htdocs/website">
+        Order allow,deny
+        Allow from all
+        Require all granted
+    </Directory>
+</VirtualHost>
+
+//Adicionar no hosts
+127.0.0.1   local.cursophp7.com.br
+
+*/
+
+?>
