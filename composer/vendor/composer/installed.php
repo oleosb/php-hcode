45f1ae11a841a94cca6420ee2c1e4515df7215d0
oleosb <oleosbo@gmail.com>
2023-09-12 14:38:21 -0300

    Composer

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hcodebr/composer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b66003cbeb6bc4cdc7ac761e465864e3f856af1c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hcodebr/composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b66003cbeb6bc4cdc7ac761e465864e3f856af1c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
